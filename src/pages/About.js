02bf8cd2e21e691a23b42c022b6a4d86585bb6f1
Aref Akminasi <137000185+Aref-Akminasi@users.noreply.github.com>
2023-09-30 11:50:55 +0200

    added basic routing, home & about pages

---
+++ src/pages/About.js
@@ -0,0 +1,29 @@
+const About = () => {
+  return (
+    <div className="about">
+      <h2>About us</h2>
+      <p>
+        Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nulla nec nibh
+        eget eros ultrices ullamcorper nec eget leo. Proin sollicitudin velit
+        faucibus lectus egestas, at auctor eros aliquet. Cras vitae turpis
+        ornare, commodo augue in, ultrices ligula. Donec in ex at erat maximus
+        congue. Etiam at dapibus lacus.
+      </p>
+      <p>
+        Nulla ac diam venenatis, elementum elit in, pharetra eros. Vivamus
+        congue erat non nibh sollicitudin ultrices. Morbi et leo ut dolor
+        consequat rhoncus scelerisque id augue. Aenean quis dignissim ex. In eu
+        feugiat urna. Nunc efficitur vel ex non vehicula.
+      </p>
+      <p>
+        Donec finibus nunc id erat eleifend auctor. Suspendisse hendrerit
+        eleifend turpis, at ultrices nulla vehicula in. Vestibulum ante ipsum
+        primis in faucibus orci luctus et ultrices posuere cubilia curae;
+        Curabitur vel rutrum purus. In hendrerit velit lorem, eu scelerisque
+        ipsum lobortis vel.
+      </p>
+    </div>
+  );
+};
+
+export default About;
---
+++ src/pages/Home.js
@@ -0,0 +1,30 @@
+const Home = () => {
+  return (
+    <div className="home">
+      <h2>Welcome</h2>
+      <p>
+        Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nulla nec nibh
+        eget eros ultrices ullamcorper nec eget leo. Proin sollicitudin velit
+        faucibus lectus egestas, at auctor eros aliquet. Cras vitae turpis
+        ornare, commodo augue in, ultrices ligula. Donec in ex at erat maximus
+        congue. Etiam at dapibus lacus.
+      </p>
+      <p>
+        Nulla ac diam venenatis, elementum elit in, pharetra eros. Vivamus
+        congue erat non nibh sollicitudin ultrices. Morbi et leo ut dolor
+        consequat rhoncus scelerisque id augue. Aenean quis dignissim ex. In eu
+        feugiat urna. Nunc efficitur vel ex non vehicula. Donec finibus nunc id
+        erat eleifend auctor.
+      </p>
+      <p>
+        Suspendisse hendrerit eleifend turpis, at ultrices nulla vehicula in.
+        Vestibulum ante ipsum primis in faucibus orci luctus et ultrices posuere
+        cubilia curae; Curabitur vel rutrum purus. In hendrerit velit lorem, eu
+        scelerisque ipsum lobortis vel. Vivamus a felis aliquet, elementum est
+        eget, finibus risus.
+      </p>
+    </div>
+  );
+};
+
+export default Home;
