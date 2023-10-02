6647c7dce04d02adb9a5a5989c6e7dfa06821525
Aref Akminasi <137000185+Aref-Akminasi@users.noreply.github.com>
2023-10-02 12:03:06 +0200

    added custom links based on the id

--- src/App.js
+++ src/App.js
@@ -11,6 +11,9 @@ import About from './pages/About';
 import Faq from './pages/help/Faq';
 import Contact from './pages/help/Contact';
 import Careers, { careersLoader } from './pages/careers/Careers';
+import CareerDetails, {
+  careerDetailsLoader,
+} from './pages/careers/CareerDetails';
 
 // layouts
 import RootLayout from './layouts/RootLayout';
@@ -30,6 +33,11 @@ const router = createBrowserRouter(
 
       <Route path="careers" element={<CareersLayout />}>
         <Route index element={<Careers />} loader={careersLoader} />
+        <Route
+          path=":id"
+          element={<CareerDetails />}
+          loader={careerDetailsLoader}
+        />
       </Route>
 
       <Route path="*" element={<NotFound />} />
---
+++ src/pages/careers/CareerDetails.js
@@ -0,0 +1,31 @@
+import { useLoaderData, useParams } from 'react-router-dom';
+
+const CareerDetail = () => {
+  const { id } = useParams();
+  const career = useLoaderData();
+  return (
+    <div className="career-details">
+      <h2>Career Details for {career.title}</h2>
+      <p>Starting salary: {career.salary}</p>
+      <p>Location: {career.location}</p>
+      <div className="details">
+        <p>
+          Lorem ipsum dolor sit amet, consectetur adipiscing elit. Proin gravida
+          metus in facilisis consectetur. In molestie porttitor rutrum. Duis
+          maximus risus non felis egestas convallis. Vestibulum et accumsan
+          urna. Vivamus quam turpis, posuere vitae massa vel, blandit iaculis
+          arcu. Proin bibendum sem turpis
+        </p>
+      </div>
+    </div>
+  );
+};
+
+export default CareerDetail;
+
+// loader function
+export const careerDetailsLoader = async ({ params }) => {
+  const { id } = params;
+  const res = await fetch('http://localhost:4000/careers/' + id);
+  return res.json();
+};
